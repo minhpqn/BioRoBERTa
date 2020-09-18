5777dbc1639858c027c950dc95e435d878c77563
minhpham <minhpham0902@gmail.com>
2020-09-18 10:52:58 +0700

    Added script for downloading PubMed dataset

---
+++ download_pubmed_abstract.py
@@ -0,0 +1,115 @@
+"""
+Download PubMed abstract files from ftp server
+PubMed Abstract 1: ftp://ftp.ncbi.nlm.nih.gov/pubmed/baseline/
+PubMed Abstract 2: ftp://ftp.ncbi.nlm.nih.gov/pubmed/updatefiles/
+"""
+import os
+import sys
+import enum
+import re
+import time
+import argparse
+import ftplib
+from ftplib import FTP
+from datetime import datetime
+from logger_utils import get_logger
+
+
+# Enum for size units
+class SIZE_UNIT(enum.Enum):
+    BYTES = 1
+    KB = 2
+    MB = 3
+    GB = 4
+
+
+def convert_unit(size_in_bytes, unit):
+    """ Convert the size from bytes to other units like KB, MB or GB"""
+    if unit == SIZE_UNIT.KB:
+        return size_in_bytes / 1024
+    elif unit == SIZE_UNIT.MB:
+        return size_in_bytes / (1024 * 1024)
+    elif unit == SIZE_UNIT.GB:
+        return size_in_bytes / (1024 * 1024 * 1024)
+    else:
+        return size_in_bytes
+
+
+def get_file_size(file_name, size_type=SIZE_UNIT.BYTES):
+    """ Get file in size in given unit like KB, MB or GB"""
+    size = os.path.getsize(file_name)
+    return convert_unit(size, size_type)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--url", type=str, required=True, help="FTP link")
+parser.add_argument("--timeout", type=float, default=60, help="Time out (seconds)")
+parser.add_argument("--skip_existing", action="store_true", help="Whether to skip existing files")
+parser.add_argument("--output_dir", type=str, default="./data", help="Path to output directory")
+args = parser.parse_args()
+
+
+
+interval = 0.05
+MIN_SIZE = 10
+
+# Get the host name
+m = re.match(r'ftp://([^/]+?)/(pubmed/[^/]+)', args.url)
+host = None
+domain = None
+if m:
+    host = m.group(1)
+    path = m.group(2)
+else:
+    raise ValueError("Invalid URL")
+
+destination = os.path.join(args.output_dir, path)
+os.makedirs(destination, exist_ok=True)
+
+print(args)
+
+LOG_FILE = os.path.join(destination, "log.txt")
+if os.path.exists(LOG_FILE):
+    os.remove(LOG_FILE)
+
+logger = get_logger(LOG_FILE, __name__)
+
+print(args.url)
+
+logger.info(f"Download files from: {args.url}")
+logger.info(f"Host: {host}")
+logger.info(f"Directory: {path}")
+
+start = datetime.now()
+ftp = FTP(host, timeout=args.timeout)
+ftp.login()
+
+try:
+    ftp.cwd(path)
+    logger.info("FPT loggin and change path sucessully!")
+except ftplib.error_perm as e:
+    logger.error("Could not change to " + path)
+    logger.error(e)
+    sys.exit("Ending Application")
+
+files = ftp.nlst()
+
+for file in files:
+    time.sleep(interval)
+    try:
+        target_file = os.path.join(destination, file)
+        if args.skip_existing and re.search(r'\.gz$', file) and os.path.isfile(target_file) and \
+                get_file_size(target_file, SIZE_UNIT.MB) > MIN_SIZE:
+            logger.info(f"Skipped file...{file}")
+        else:
+            logger.info("Downloading..." + file)
+            ftp.retrbinary("RETR " + file, open(target_file, "wb").write)
+    except ftplib.error_perm as e:
+        logger.error("File could not be downloaded " + file)
+        logger.info(e)
+
+ftp.close()
+
+end = datetime.now()
+diff = end - start
+logger.info("All files downloaded for ' + str(diff.seconds) + 's'")
